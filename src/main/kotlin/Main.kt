880b32a00bcf1ad1b9f759b0a1356578c37575d0
shmueldabomb441 <60489781+shmueldabomb441@users.noreply.github.com>
2022-06-10 17:15:34 -0400

    Update local json and add getting from web functionality(WIP)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,21 @@
 import com.google.gson.Gson
 import java.io.File
+import java.net.URL
+import java.time.LocalDate
 import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
-val json = Gson().fromJson(
-    File("/Users/shmuel/IdeaProjects/ParseWakaTime/src/wakatime-software.sternbachgmail.com-1dc3ae8aa0b040d39cc8d00842150ee5.json").readText(),
-    JSON::class.java
-)
+    val startDate = "2021-09-10"
+    val endDate = LocalDate.now().toString()
+    val json = Gson().fromJson(
+        getJSON(
+            false,
+            File("/Users/shmuel/IdeaProjects/ParseWakaTime/src/wakatime-software.sternbachgmail.com-1dc3ae8aa0b040d39cc8d00842150ee5.json"),
+            startDate,
+            endDate
+        ),
+        JSON::class.java
+    )
     println(
         json
             .days
@@ -18,6 +27,11 @@ val json = Gson().fromJson(
             .toHrMinSec()
     )
 }
+
+private fun getJSON(getFromWeb: Boolean, file: File?, startDate: String?, endDate: String?) = if (getFromWeb)
+    URL("https://wakatime.com/api/v1/users/current/summaries?start=${startDate}&end=$endDate?access_token=66142ee1-2ce3-4bb6-a8a7-7d685219f78e").readText()
+else file!!.readText()
+
 fun Int.toHrMinSec(hour: Int = 0, minute: Int = 0, second: Int = this): Triple<Int, Int, Int> {
     var minute1 = minute
     var second1 = second
